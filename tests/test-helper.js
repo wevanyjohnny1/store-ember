e4a35acb1b17cdfc3e48cf9efdf384949b0f6124
johnny <johnnywevany@outlook.com>
2021-08-02 19:55:58 -0300

    starting project

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'store-ember/app';
+import config from 'store-ember/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
