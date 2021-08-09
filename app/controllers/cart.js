ed95f82258327e13af6976dd43154affc2d448dc
johnny <johnnywevany@outlook.com>
2021-08-09 17:11:26 -0300

    adding

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,9 +1,13 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class CartController extends Controller {
+  @service('shopping-cart') cart;
+
   get subtotal() {
-    return this.model.reduce((acc, item) => {
-      return acc + item.price;
+    return this.cart.itemList.reduce((acc, item) => {
+      return acc + item.price * item.count;
     }, 0);
   }
   get desconto() {
@@ -12,5 +16,12 @@ export default class CartController extends Controller {
 
   get total() {
     return this.subtotal - this.desconto;
+  }
+
+  @action
+  updateItemCount(item, event) {
+    const count = event.target.value;
+
+    count >= 0 ? (item.count = count) : (item.count = 0);
   }
 }
