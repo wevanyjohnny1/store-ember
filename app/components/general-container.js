5fa265b2ea3d3742510cedf72318b8c9888c509d
johnny <johnnywevany@outlook.com>
2021-08-09 19:19:13 -0300

    tests added

--- app/components/general-container.js
+++ app/components/general-container.js
@@ -2,10 +2,10 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
 export default class GeneralContainerComponent extends Component {
-  @service('shopping-cart') cart;
+  @service shoppingCart;
 
   get itemCount() {
-    return this.cart.itemList.reduce((total, item) => {
+    return this.shoppingCart.itemList.reduce((total, item) => {
       return (total += item.count);
     }, 0);
   }
